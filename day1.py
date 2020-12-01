4e8e41f946b29f38f7705aeb6897d46ea68f5215
azaliyaraisovna <azaliyaraisovna@gmail.com>
2020-12-01 22:57:49 +0300

    day 1

---
+++ day1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[39]:
+
+
+## Advent of code
+##Day 1
+A = input().split()
+for i in range(len(A)):
+    A[i] = int(A[i])
+
+for i in range(len(A)):
+    for j in range(len(A)):
+        if i != j and A[i] + A[j] == 2020:
+            m = A[i] * A[j]
+print(m)
+
+for i in range(len(A)):
+    for j in range(len(A)):
+        for k in range(len(A)):
+            if i != j and j != k and A[i] + A[j] + A[k] == 2020:
+                m3 = A[i] * A[j] * A[k]
+print(m3)
+
